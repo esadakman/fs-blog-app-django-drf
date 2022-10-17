e3eff18a22e10a4f0fdf9e5dca5f92b72d55b2fd
esadakman <esadd26@gmail.com>
2022-10-17 23:00:47 +0300

    profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
+class UserModel(User):
+    @property 
+    def my_profile(self):
+        return self.profile
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(default="default.webp", upload_to="profile_pics")
+
+    def __str__(self):
+        return f"{self.user.username} Profile"
+
+
+    def save(self, *args, **kwargs):  # ! this function already exists in our super(), we are ovveriding it to make sure images are not too big
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers, validators
 from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.hashers import make_password
+from .models import Profile
 from dj_rest_auth.serializers import TokenSerializer
 
 
@@ -9,7 +11,8 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(
         required=True,
-        validators=[validators.UniqueValidator(queryset=User.objects.all())]
+        validators=[UniqueValidator(queryset=User.objects.all(),  # ? for unique emails
+                                    message='This email has been used.')]
     )
 
     password = serializers.CharField(
@@ -28,6 +31,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            # 'id',
             'username',
             'first_name',
             'last_name',
@@ -38,17 +42,10 @@ class RegisterSerializer(serializers.ModelSerializer):
 
         extra_kwargs = {
             "password": {"write_only": True},
-            "password2": {"write_only": True}
+            "password2": {"write_only": True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
         }
-
-    def create(self, validated_data):
-        password = validated_data.get("password")
-        validated_data.pop("password2")
-
-        user = User.objects.create(**validated_data)
-        user.password = make_password(password)
-        user.save()
-        return user
 
     def validate(self, data):
         if data["password"] != data["password2"]:
@@ -56,11 +53,25 @@ class RegisterSerializer(serializers.ModelSerializer):
                 {"password": "Password fields didn't match."})
         return data
 
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
 
 class UserTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name')
+        fields = ('username',
+                  'email')
 
 
 class CustomTokenSerializer(TokenSerializer):
@@ -69,3 +80,33 @@ class CustomTokenSerializer(TokenSerializer):
 
     class Meta(TokenSerializer.Meta):
         fields = ('key', 'user')
+
+
+class ProfileUpdateForm(serializers.ModelSerializer):
+    user = UserTokenSerializer()
+
+    class Meta:
+        model = Profile
+        fields = (
+            'image',
+            'user'
+        )
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+        user = instance.user
+
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+
+        user.username = user_data.get(
+            'username',
+            user.username
+        )
+        user.email = user_data.get(
+            'email',
+            user.email
+        )
+        user.save()
+
+        return instance
