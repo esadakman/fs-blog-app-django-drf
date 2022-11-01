27c4e3bd0bbf115284e78f3c549223ce0114844c
esadakman <esadd26@gmail.com>
2022-11-01 16:30:15 +0300

    userTtoken serializer updated, profile permission added

--- users/models.py
+++ users/models.py
@@ -1,26 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-
+# from PIL import Image
 class UserModel(User):
-    @property 
+    @property
     def my_profile(self):
         return self.profile
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')  
-    image = models.ImageField(default="default.webp", upload_to="profile_pics")
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    image = models.URLField(max_length=300, blank=True,
+                            default="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png")
 
     def __str__(self):
         return f"{self.user.username} Profile"
 
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
 
-    def save(self, *args, **kwargs):  
-        super().save(*args, **kwargs)
+    #     img = Image.open(self.image.path)
 
-        img = Image.open(self.image.path)
-
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.image.path)
